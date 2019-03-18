00a63bdef502fb45932a5545be507f5ea2750eab
RDx0811 <43957261+RDx0811@users.noreply.github.com>
2019-03-18 21:27:17 +0530

    Add files via upload

---
+++ UNO.py
@@ -0,0 +1,101 @@
+#uno
+import random
+
+def fill(l):
+    colour='R'
+    for i in range(10):
+        l.append([colour,str(i)])
+    colour='B'
+    for i in range(10):
+        l.append([colour,str(i)])
+    colour='Y'
+    for i in range(10):
+        l.append([colour,str(i)])
+    colour='G'
+    for i in range(10):
+        l.append([colour,str(i)])
+    random.shuffle(l)
+    return l
+
+def start(l):
+    for i in range(7):
+        r=random.choice(deck)
+        l.append(r)
+        deck.remove(r)
+    return l
+
+def display():
+    print("My Deck:")
+    for i in range(len(me)):
+        print(i,end='\t')
+    print()
+    for c in me:
+        print(''.join(c),end='\t')
+    print('\n')
+
+def play(player,card):
+    chosen=0
+    for c in player:
+        if c[0]==card[0] or c[1]==card[1]:
+            card=c
+            player.remove(card)
+            print(turn,''.join(card))
+            return card
+    r=deck[0]
+    player.append(r)
+    deck.remove(r)
+    print(turn,"T, Card: ",''.join(card))
+    return card
+
+
+
+#Setup
+deck=[]
+fill(deck)
+players=['Me: ','P1: ']
+me=[]
+start(me)
+player1=[]
+start(player1)
+card=random.choice(deck)
+deck.remove(card)
+print('Start: ',''.join(card))
+t_pos=0
+turn=players[t_pos]
+
+#Gameplay
+while(len(me)!=0 and len(player1)!=0):
+    if turn=='Me: ':
+        display()
+        #Check for valid input
+        valid=0
+        while valid==0:
+            p=input("Enter your choice: ")
+            if p=='T' or me[int(p)][0]==card[0] or me[int(p)][1]==card[1]:
+                valid=1
+            else:
+                print("Invalid Choice")
+        if p=='T':
+            r=deck[0]
+            me.append(r)
+            deck.remove(r)
+            print(turn,"T")
+        else:
+            p=int(p)
+            card=me[p]
+            print(turn,''.join(card))
+            me.remove(card)
+    elif turn=='P1: ':
+        card=play(player1,card)
+    #Next Player
+    if t_pos==len(players)-1:
+        t_pos=0
+    else:
+        t_pos+=1
+    turn=players[t_pos]
+
+#Winner
+if len(me)==0:
+    print("You Win!")
+elif len(player1)==0:
+    print("Player 1 wins!")
