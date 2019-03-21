965a88f85a03b93993c6eddd3afb49745e4ef74d
santosh50 <47947735+santosh50@users.noreply.github.com>
2019-03-21 18:04:18 +0530

    Add files via upload

--- UNO.py
+++ UNO.py
@@ -38,8 +38,12 @@ def play(player,card):
     for c in player:
         if c[0]==card[0] or c[1]==card[1]:
             card=c
+            print(turn,''.join(card),end='')
             player.remove(card)
-            print(turn,''.join(card))
+            used.append(card)
+            if len(player)==1:
+                print(", UNO!")
+            print()
             return card
     r=deck[0]
     player.append(r)
@@ -47,16 +51,35 @@ def play(player,card):
     print(turn,"T, Card: ",''.join(card))
     return card
 
+def choose_colour(player):
+    r=0;b=0;y=0;g=0
+    for i in player:
+        if i[0]=='R':
+            r+=1
+        elif i[0]=='B':
+            b+=1
+        if i[0]=='Y':
+            y+=1
+        if i[0]=='G':
+            g+=1
+    choice=max(r,b,y,g)
+        
+        
+    
+        
+            
 
-
-#Setup
+#Setup    
 deck=[]
 fill(deck)
-players=['Me: ','P1: ']
+used=[]
+players=['Me: ','P1: ','P2: ']
 me=[]
 start(me)
 player1=[]
 start(player1)
+player2=[]
+start(player2)
 card=random.choice(deck)
 deck.remove(card)
 print('Start: ',''.join(card))
@@ -64,7 +87,7 @@ t_pos=0
 turn=players[t_pos]
 
 #Gameplay
-while(len(me)!=0 and len(player1)!=0):
+while len(me)!=0 and len(player1)!=0 and len(player2)!=0:
     if turn=='Me: ':
         display()
         #Check for valid input
@@ -83,19 +106,41 @@ while(len(me)!=0 and len(player1)!=0):
         else:
             p=int(p)
             card=me[p]
-            print(turn,''.join(card))
+            print(turn,''.join(card),end='')
             me.remove(card)
+            used.append(card)
+            if len(me)==1:
+                print(", UNO!")
+            print()
     elif turn=='P1: ':
-        card=play(player1,card)
+      card=play(player1,card)
+    elif turn=='P2: ':
+      card=play(player2,card)
     #Next Player
     if t_pos==len(players)-1:
         t_pos=0
     else:
         t_pos+=1
     turn=players[t_pos]
+    if len(deck)==0:
+        random.shuffle(used)
+        deck+=used
+        used=[]
 
 #Winner
 if len(me)==0:
-    print("You Win!")
+      print("You Win!")
 elif len(player1)==0:
-    print("Player 1 wins!")
+      print("Player 1 wins!")
+elif len(player2)==0:
+      print("Player 2 wins!")
+
+
+        
+
+
+
+
+
+
+        
